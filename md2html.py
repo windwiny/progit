fd6bbcd93c1d92405ba3f6e57730ad1daf62f301
windwiny <windwiny.ubt@gmail.com>
2012-02-28 14:39:08 +0800

    md2html

--- md2html.py
+++ md2html.py
@@ -11,64 +11,119 @@ import os
 import re
 import markdown
 
-langs = ['zh']
-if len(sys.argv) > 1:
-    for d in sys.argv[1:]:
-        if os.path.isdir(d):
-            langs.append(d)
 
-html = '''
-<!DOCTYPE html>
+INDEX = '''<!DOCTYPE html>
 <html>
   <head>
-    <meta content="text/html; charset=UTF-8" http-equiv="content-type">
-    <title>%(title)s</title>
-    <link rel=stylesheet type="text/css" href="./epub/master.css">
+    <meta content="text/html; charset=UTF-8" http-equiv="Content-Type">
+    <title>Pro Git</title>
+  </head>
+  <frameset cols="20%,80%">
+    <frame src="directory.html">
+    <frame name="content"src="ch1-0.html">
+    <noframes>
+      <body>
+      <p>This page uses frames. The current browser you are using does not support frames.</p>
+      </body>
+    </noframes>
+  </frameset>
+</html>'''
+
+DIRECTORY = '''<!DOCTYPE html>
+<html>
+  <head>
+    <meta content="text/html; charset=UTF-8" http-equiv="Content-Type">
+    <title>Pro Git</title>
+  </head>
+  <body>
+    %s
+  </body>
+</html>'''
+
+HTML = '''<!DOCTYPE html>
+<html>
+  <head>
+    <meta content="text/html; charset=UTF-8" http-equiv="Content-Type">
+    <title>%s</title>
+    <link rel=stylesheet type="text/css" href="../epub/master.css">
   </head>
   <body>
     <div id="wrapper">
     <div id="content">
-    %(body)s
+    %s
     </div>
     </div>
   </body>
 </html>
 '''
 
+def writefile(filename, txt):
+    if os.path.isfile(filename) and open(filename, 'r').read() == txt:
+        print ' -> %s  \t some as old, skip ..' % filename
+    else:
+        open(filename, 'w').write(txt)
+        print ' -> %s' % filename
+
 def md2html(lang):
-    ins = re.compile(r'\s*Insert\s+(\d+fig\d+).png')
-    for root,dirs,files in os.walk(lang):
+    htmldir = 'html_%s' % lang
+    if not os.path.isdir(htmldir):
+        os.mkdir(htmldir)
+
+    charpter, section = 1, 0
+    htmlfiles = []
+    for root, dirs, files in os.walk(lang):
         for name in files:
             if name.endswith('.markdown'):
                 src = os.path.join(root, name)
-                dst = '%s-%s.html' % (lang, name)
-                if os.path.isfile(dst) and '-f' not in sys.argv and os.stat(dst).st_mtime > os.stat(src).st_mtime:
-                    print "%s newer %s, skip.. \t(or add '-f' param)" % (dst, src)
-                    continue
-                if not os.path.isdir('tmp/%s' % root):
-                    os.makedirs('tmp/%s' % root)
-                f1 = open(src, 'r')
-                f2 = open('tmp/%s' % src, 'w')
-                for line in f1:
-                    matches = ins.match(line)
-                    if matches:
-                        n = 'figures/%s-tn.png' % matches.group(1)
-                        line = "![%s](%s)\n" % (n, n)
-                    f2.write(line)
-                f1.close()
-                f2.close()
 
-                title = dst
-                body = markdown.markdown(open(f2.name,'r').read().decode('utf-8')).encode('utf-8')
-                open(dst, 'w').write(html % locals())
-                print '-> %s' % dst
-    
-def main():
-    if not os.path.isdir('tmp'):
-        os.mkdir('tmp')
+                data = open(src, 'r').read()
+                # replace image
+                data = re.sub(r'\s*Insert\s+(\d+fig\d+).png',
+                              r"![figures/\1-tn.png](../figures/\1-tn.png)\n",
+                              data,
+                              re.IGNORECASE)
+
+                # markdown text to html text
+                htmltxt = markdown.markdown(data.decode('utf-8')).encode('utf-8')
+
+                # split charpter section
+                for body in htmltxt.split('<h2>'):
+                    body = '<h2>' + body
+                    title = re.search(r'<h(\d)>(.*?)</h\1>', body).groups()[1]
+                    dst = '%s/ch%d-%d.html' % (htmldir, charpter, section)
+                    htmlfiles.append((dst, title, charpter, section))
+                    txts = HTML % (title, body)
+                    writefile(dst, txts)
+                    section += 1
+                charpter += 1
+                section = 0
+
+    # create directory.html
+    htmllinks = []
+    for fn, title, ch, se in htmlfiles:
+        fn = os.path.split(fn)[1]
+        if se == 0:
+            htmllinks.append('<br /><b><a href="%s" target="content">%d %s</a></b>' % (fn, ch, title))
+        else:
+            htmllinks.append('<br />&nbsp;&nbsp;<a href="%s" target="content">%d.%d %s</a>' % (fn, ch, se, title))
+    writefile('%s/directory.html' % htmldir, DIRECTORY % '\n'.join(htmllinks))
+
+    # create index.html
+    writefile('%s/index.html' % htmldir, INDEX)
+
+def main(langs):
     for lang in langs:
         print '%s\nGenerating %s html files\n%s' % ('--' * 30, lang, '--' * 30)
         md2html(lang)
 
 if __name__ == '__main__':
-    main()
+    langs = []
+    if len(sys.argv) > 1:
+        for d in sys.argv[1:]:
+            if os.path.isdir(d):
+                langs.append(d)
+    
+    if langs:
+        main(langs)
+    else:
+        print 'Syntax:\n  %s langdir [langdir ..] ' % os.path.basename(sys.argv[0])
